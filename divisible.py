162343ce6707ec686b50d7a2d6ffa85baa1c098a
darksnake <93006390+darksnakepy@users.noreply.github.com>
2021-12-06 12:54:51 +0100

    Create divisible.py

---
+++ divisible.py
@@ -0,0 +1,8 @@
+#CHECKS IF IN THE NUM_LIST THERE IS A NUMBER DIVISIBLE FOR 5
+
+num_list = [10, 20, 33, 46, 55]
+print("Given list:", num_list)
+print('Divisible by 5:')
+for num in num_list:
+    if num % 5 == 0:
+        print(num)
