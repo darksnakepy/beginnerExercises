758a098b3d30edec8d0f70c3ec2ffa1e6e4a89ef
darksnake <93006390+darksnakepy@users.noreply.github.com>
2022-05-28 16:43:59 +0200

    generator
    MAC address generator

---
+++ macAddrGen.py
@@ -0,0 +1,17 @@
+import random
+
+
+char_set = "ABCDEF0123456789"
+mac_addr = ""
+dot = 0
+
+for _ in range(6):
+    for _ in range(2):
+        mac_addr += random.choice(char_set)
+        if dot < 5:
+          mac_addr += ":"
+          dot += 1
+          print("mac address: ", mac_addr)
+          input()
+
+
